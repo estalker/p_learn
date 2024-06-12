cbf1e0bd93b838686109506a5ffaaeea93e8e7fa
estalker <sergei.knyazev@gmail.com>
2024-06-12 12:53:44 +0300

    homework2 done

--- module2/homework2.py
+++ module2/homework2.py
@@ -2,4 +2,9 @@ first = input()
 second = input()
 third = input()
 
-if first ==
+if first == second and second == third:
+    print(3)
+elif first == second or second == third or first == third:
+    print(2)
+else:
+    print(0)
