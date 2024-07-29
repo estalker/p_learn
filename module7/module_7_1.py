93e74e225afa7e1605da27558d11964fdc3e0675
estalker <sergei.knyazev@gmail.com>
2024-07-29 12:47:07 +0300

    Задача из модуля 7

--- module7/module_7_1.py
+++ module7/module_7_1.py
@@ -1,4 +1,6 @@
 import os
+
+
 class Product:
     def __init__(self, name: str, weight: float, category: str):
         self.name = name
@@ -13,7 +15,7 @@ class Shop:
     __file_name = 'products.txt'
 
     def get_products(self):
-        f = open(Shop.__file_name,"r")
+        f = open(Shop.__file_name, "r")
         text = f.read()
         f.close()
         return text
@@ -21,7 +23,7 @@ class Shop:
     def add(self, *products):
         if not os.path.exists(Shop.__file_name):
             open(Shop.__file_name, 'w').close()
-        f = open(self.__file_name,"r")
+        f = open(self.__file_name, "r")
         lines = f.readlines()
         f.close()
 
@@ -38,14 +40,12 @@ class Shop:
         fw.close()
 
 
-
-
 s1 = Shop()
 p1 = Product('Potato', 50.5, 'Vegetables')
 p2 = Product('Spaghetti', 3.4, 'Groceries')
 p3 = Product('Potato', 5.5, 'Vegetables')
 
-print(p2) # __str__
+print(p2)  # __str__
 
 s1.add(p1, p2, p3)
 
