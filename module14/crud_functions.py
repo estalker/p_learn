05911419a9123834db82d1b63bf9730c9e611189
estalker <sergei.knyazev@gmail.com>
2024-10-17 13:06:18 +0300

    Задача 4 из модуля 14 (ver 1)

---
+++ module14/crud_functions.py
@@ -0,0 +1,31 @@
+import sqlite3
+
+
+def initiate_db():
+    conn = sqlite3.connect("not_telegram.db")
+    cursor = conn.cursor()
+
+    cursor.execute('''CREATE TABLE IF NOT EXISTS Products(
+        id INTEGER PRIMARY KEY,
+        title TEXT NOT NULL,
+        description TEXT,
+        price INTEGER NOT NULL
+        )
+    ''')
+
+    conn.commit()
+    conn.close()
+
+
+def get_all_products():
+    conn = sqlite3.connect("not_telegram.db")
+    cursor = conn.cursor()
+
+    cursor.execute('''SELECT * FROM Products''')
+
+    products = cursor.fetchall()
+
+    conn.commit()
+    conn.close()
+
+    return products
