0a91ab54725dba0b2ca8f11026fce651c842484f
estalker <sergei.knyazev@gmail.com>
2024-10-21 19:40:36 +0300

    Задача 5 из модуля 14 (ver 1)

--- module14/crud_functions.py
+++ module14/crud_functions.py
@@ -13,6 +13,15 @@ def initiate_db():
         )
     ''')
 
+    cursor.execute('''CREATE TABLE IF NOT EXISTS Users(
+           id INTEGER PRIMARY KEY,
+           username TEXT NOT NULL,
+           email  TEXT NOT NULL,
+           age INTEGER NOT NULL,
+           balance INTEGER NOT NULL
+           )
+       ''')
+
     conn.commit()
     conn.close()
 
@@ -29,3 +38,29 @@ def get_all_products():
     conn.close()
 
     return products
+
+
+def add_user(username, email, age):
+    conn = sqlite3.connect("not_telegram.db")
+    cursor = conn.cursor()
+
+    cursor.execute('''INSERT INTO Users(username, email, age, balance) VALUES(?,?,?,?)''', (username, email, age, 1000))
+
+    conn.commit()
+    conn.close()
+
+
+def is_included(username):
+    conn = sqlite3.connect("not_telegram.db")
+    cursor = conn.cursor()
+
+    cursor.execute('''SELECT COUNT(*) FROM Users WHERE username = ?''', (username,))
+
+    count = cursor.fetchone()[0]
+
+    conn.commit()
+    conn.close()
+
+    if count == 0:
+        return False
+    return True
---
+++ module14/module_14_5.py
@@ -0,0 +1,145 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types, F
+from aiogram.filters.command import Command
+from aiogram.filters import Filter
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton,ReplyKeyboardRemove
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from crud_functions import *
+
+
+key = open('token_key.secret', 'r').read()
+
+bot = Bot(token=key)
+dp = Dispatcher(storage=MemoryStorage())
+
+kb = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text="Рассчитать"),
+                                    KeyboardButton(text="Информация"),
+                                    KeyboardButton(text="Купить"),
+                                    KeyboardButton(text="Регистрация")]],
+    resize_keyboard=True)
+
+kbi = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="Рассчитать норму калорий", callback_data='calories'),
+     InlineKeyboardButton(text='Формулы расчёта', callback_data='formulas')]], resize_keyboard=True)
+
+kbi_products = InlineKeyboardMarkup(inline_keyboard=[
+    [
+        InlineKeyboardButton(text="Продукт 1", callback_data='product_buying'),
+        InlineKeyboardButton(text='Продукт 2', callback_data='product_buying'),
+        InlineKeyboardButton(text="Продукт 3", callback_data='product_buying'),
+        InlineKeyboardButton(text='Продукт 4', callback_data='product_buying'),
+     ]],
+    resize_keyboard=True)
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
+class RegistrationState(StatesGroup):
+    username = State()
+    email = State()
+    age = State()
+    balance = State()
+
+@dp.message(Command("start"))
+async def start(message: types.Message):
+    await message.answer("Привет! Я бот помогающий твоему здоровью.", reply_markup=kb)
+
+
+@dp.message(F.text == "Рассчитать")
+async def main_menu(message):
+    await message.answer("Выберите опцию:", reply_markup=kbi)
+
+
+@dp.message(F.text == "Купить")
+async def get_buying_list(message):
+    for r in get_all_products():
+        await message.answer_photo(types.FSInputFile(path=f"p{r[0]}.png"), f"Название: {r[1]} | Описание: {r[2]} | Цена: {r[3]}")
+
+    await message.answer("Выберите продукт для покупки:", reply_markup=kbi_products)
+
+
+@dp.message(F.text == "Регистрация")
+async def sing_up(message, state):
+    await message.answer("Введите имя пользователя (только латинский алфавит):")
+    await state.set_state(RegistrationState.username)
+
+
+@dp.message(RegistrationState.username)
+async def set_username(message, state):
+    if not is_included(message.text):
+        await state.update_data(username=message.text)
+        await message.answer("Введите свой email:")
+        await state.set_state(RegistrationState.email)
+    else:
+        await message.answer("Пользователь существует, введите другое имя")
+        await state.set_state(RegistrationState.username)
+
+
+@dp.message(RegistrationState.email)
+async def set_email(message, state):
+    await state.update_data(email=message.text)
+    await message.answer("Введите свой возраст:")
+    await state.set_state(RegistrationState.age)
+
+
+@dp.message(RegistrationState.age)
+async def set_email(message, state):
+    await state.update_data(age=message.text)
+    data = await state.get_data()
+    add_user(data['username'], data['email'], data['age'])
+    await message.answer("Регистрация прошла успешно")
+@dp.callback_query(F.data == 'product_buying')
+async def send_confirm_message(call):
+    await call.message.answer("Вы успешно приобрели продукт!")
+    await call.answer()
+
+
+@dp.callback_query(F.data == 'formulas')
+async def get_formulas(call):
+    await call.message.answer("10 х вес (кг) + 6,25 x рост (см) – 5 х возраст (г) + 5")
+    await call.answer()
+
+
+@dp.callback_query(F.data == 'calories')
+async def set_age(call, state):
+    await call.message.answer("Введите свой возраст:")
+    await state.set_state(UserState.age)
+    await call.answer()
+
+
+@dp.message(UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age=message.text)
+    await message.answer("Введите свой рост:")
+    await state.set_state(UserState.growth)
+
+
+@dp.message(UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth=message.text)
+    await message.answer("Введите свой вес:")
+    await state.set_state(UserState.weight)
+
+
+@dp.message(UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(weight=message.text)
+    data = await state.get_data()
+    cals = 10 * float(data['weight']) + 6.25 * float(data['growth']) - 5 * float(data['age']) + 5;
+    await message.answer(f"Ваша норма калорий {cals}", reply_markup=ReplyKeyboardRemove())
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    initiate_db()
+    asyncio.run(main())
+
+
