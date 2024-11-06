899156e73405ca91dd870d57ffb798d05f76e838
estalker <sergei.knyazev@gmail.com>
2024-11-06 16:33:15 +0300

    Задача 4 из модуля 16 (ver 1)

---
+++ module16/module_16_4.py
@@ -0,0 +1,60 @@
+import uvicorn
+from fastapi import FastAPI, Path, HTTPException
+from typing import Annotated, List, Any
+from pydantic import BaseModel
+
+
+class User(BaseModel):
+    id: int
+    username: str
+    age: int
+
+
+app = FastAPI()
+
+users = []
+
+
+@app.get("/users")
+async def get_users() -> List[User]:
+    return users
+
+
+@app.post("/user/{username}/{age}'")
+async def post_user(username: Annotated[
+    str, Path(..., description="Enter username", min_length=5, mx_length=20, example="UrbanUser")],
+                    age: Annotated[int, Path(..., description="Enter age", ge=18, le=120, example=24)]) -> User:
+    current_id = int(max(tuple(user.id for user in users), default=0)) + 1
+    new_user = User(id=current_id, username=username, age=age)
+    users.append(new_user)
+    return new_user
+
+
+@app.put("/user/{user_id}/{username}/{age}")
+async def update_user(user_id: Annotated[int, Path(..., description="Enter user_id", ge=1, le=9999, example=1)],
+                      username: Annotated[str, Path(..., description="Enter username", min_length=5, mx_length=20,
+                                                    example="UrbanUser")],
+                      age: Annotated[int, Path(..., description="Enter age", ge=18, le=120, example=24)]) -> User:
+    try:
+        user_index = [index for index in range(len(users)) if users[index].id == user_id][0]
+        user = users[user_index]
+        user.username = username
+        user.age = age
+        return user
+    except IndexError:
+        raise HTTPException(status_code=404, detail="User is not found")
+
+
+@app.delete("/user/{user_id}")
+async def delete_user(
+        user_id: Annotated[int, Path(..., description="Enter user_id", ge=1, le=9999, example=1)]) -> User:
+    try:
+        user_index = [index for index in range(len(users)) if users[index].id == user_id][0]
+        user = users.pop(user_index)
+        return user
+    except  IndexError:
+        raise HTTPException(status_code=404, detail="User is not found")
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
