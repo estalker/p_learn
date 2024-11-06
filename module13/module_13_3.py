ec95658bd3cf69e6e408b25a5d14ab09631f4652
estalker <sergei.knyazev@gmail.com>
2024-11-06 13:07:28 +0300

    Задача 3 из модуля 16 (ver 1)

--- module13/module_13_3.py
+++ module13/module_13_3.py
@@ -2,7 +2,7 @@ import asyncio
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 
-key = open('token_key.secret','r').read()
+key = open('token_key.secret', 'r').read()
 
 bot = Bot(token=key)
 dp = Dispatcher()
@@ -10,13 +10,14 @@ dp = Dispatcher()
 
 @dp.message(Command("start"))
 async def start(message: types.Message):
-    await message.answer("Привет! Я бот помогающий твоему здоровью.")
-    #print("Привет! Я бот помогающий твоему здоровью.")
+    await message.answer(
+        "Привет! Я бот помогающий твоему здоровью.")  #print("Привет! Я бот помогающий твоему здоровью.")
+
 
 @dp.message()
 async def all_massages(message: types.Message):
-    await message.reply("Введите команду /start, чтобы начать общение.")
-    #print("Введите команду /start, чтобы начать общение.")
+    await message.reply(
+        "Введите команду /start, чтобы начать общение.")  #print("Введите команду /start, чтобы начать общение.")
 
 
 async def main():
