6c28a3052495c41d4fa97342a0eb98fdca76a216
estalker <sergei.knyazev@gmail.com>
2024-11-13 14:13:58 +0300

    Задача 2 из модуля 17 (ver 1)

---
+++ module17/app/models/user.py
@@ -0,0 +1,21 @@
+from module17.app.backend.db import Base
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+
+from module17.app.models import *
+class User(Base):
+    __tablename__ = "users"
+    __table_args__ = {"extend_existing": True}
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    firstname = Column(String)
+    lastname = Column(String)
+    content = Column(String)
+    age = Column(Integer)
+    slug = Column(String, unique=True, index=True)
+    tasks = relationship("Task", back_populates="user")
+
+
+
+from sqlalchemy.schema import CreateTable
+print(CreateTable(User.__table__))
