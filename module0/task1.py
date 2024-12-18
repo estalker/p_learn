4278e47f0a12241a1ff65a912b0119f73e100026
estalker <sergei.knyazev@gmail.com>
2024-12-18 16:17:29 +0300

    Задача 1 из модуля 0 (ver 1)

---
+++ module0/task1.py
@@ -0,0 +1,19 @@
+# 1st program
+print(9 ** 0.5 * 5)
+
+
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+no_priority = 2 * 2 + 2
+priority = 2 * (2 + 2)
+print(no_priority == priority)
+
+# 4th program
+s = '123.456'
+f = float(s)
+f10 = f * 10
+i = int(f10)
+print(i % 10)
+
